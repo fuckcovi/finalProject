87ee7becbeb95eda84c04c9511f0089cd392bace
user1 <user1@user1-PC>
2017-07-27 18:23:08 +0900

    aaa

--- Final_MIXMATCH_old/src/main/java/com/kh/mixmatch/member/controller/LoginController.java
+++ Final_MIXMATCH_old/src/main/java/com/kh/mixmatch/member/controller/LoginController.java
@@ -1,16 +1,68 @@
 package com.kh.mixmatch.member.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kh.mixmatch.member.domain.MemberCommand;
+import com.kh.mixmatch.member.service.MemberService;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/login.do")
+private Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource
+	private MemberService memberService;
+	
+	@ModelAttribute("memberCommand")
+	public MemberCommand initCommand(){
+		return new MemberCommand();
+	}
+	
+	@RequestMapping(value="/login.do",method=RequestMethod.GET)
 	public String loginForm(){
 		return "login";
+	}
+	
+	@RequestMapping(value="/login.do",method=RequestMethod.POST)
+	public String loginAction(@ModelAttribute("memberCommand")@Valid MemberCommand memberCommand, BindingResult result, HttpSession session){
+		if(log.isDebugEnabled()){
+			log.debug("<<memberCommand>> : " + memberCommand);
+		}
+		
+		if(result.hasFieldErrors("id") || result.hasFieldErrors("pw")){
+			return loginForm();
+		}
+		
+		try {
+			MemberCommand member = memberService.selectMember(memberCommand.getId());
+			
+			boolean check = false;
+			
+			if(member != null){
+				check = member.isCheckedPasswd(memberCommand.getPw());
+			}
+			
+			if(check){
+				session.setAttribute("user_id", memberCommand.getId());
+				session.setAttribute("auth", memberCommand.getAuth());
+				
+				return "redirect:/main.do";
+			}else{
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			result.reject("invalidIdOrPassword");
+			return loginForm();
+		}
 	}
 	
 	@RequestMapping("/logout.do")
