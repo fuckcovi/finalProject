60cfc5f552569f7a3b15e55ed748ed1b9124a619
khmixmatch <koff777@naver.com>
2017-08-02 18:23:28 +0900

    fff

--- Final_MIXMATCH_old/src/main/java/com/kh/mixmatch/team/dao/TotalTypeMapper.java
+++ Final_MIXMATCH_old/src/main/java/com/kh/mixmatch/team/dao/TotalTypeMapper.java
@@ -12,7 +12,7 @@ import com.kh.mixmatch.team.domain.BasketCommand;
 import com.kh.mixmatch.team.domain.FootCommand;
 
 public interface TotalTypeMapper {
-	@Insert("INSERT INTO g_baseball(b_seq,id,t_name,m_seq,b_bat,b_hit,b_rbi,b_score,b_avg,b_win,b_lose,b_strike,b_ip,b_er,b_era) VALUES(f_baseball_seq.nextval,#{id},#{t_name},#{m_seq},#{b_bat},#{b_hit},#{b_rbi},#{b_score},#{b_avg},#{b_win},#{b_lose},#{b_strike},#{b_ip},#{b_er},#{b_era})")
+	@Insert("INSERT INTO g_baseball(b_seq,id,t_name,m_seq,b_bat,b_hit,b_rbi,b_score,b_avg,b_win,b_lose,b_strike,b_ip,b_er,b_era) VALUES(f_baseball_seq.nextval,#{id},#{t_name},#{m_seq},#{b_bat},#{b_hit},#{b_rbi},#{b_score},NVL(#{b_hit}/DECODE(#{b_bat},0,NULL,#{b_bat}),0),#{b_win},#{b_lose},#{b_strike},#{b_ip},#{b_er},NVL((#{b_er}*9)/DECODE(#{b_ip},0,NULL,#{b_ip}),0))")
 	public void insertBase(BaseCommand base);
 	@Insert("INSERT INTO g_basketball(b_seq,id,t_name,m_seq,b_score,b_assist,b_rebound,b_steel,b_block,b_3point) VALUES(f_basketball_seq.nextval,#{id},#{t_name},#{m_seq},#{b_score},#{b_assist},#{b_rebound},#{b_steel},#{b_block},#{b_3point})")
 	public void insertBasket(BasketCommand basket);
@@ -23,7 +23,7 @@ public interface TotalTypeMapper {
 	public void deleteBasket(Integer b_seq);
 	public void deleteFoot(Integer f_seq);
 	
-	@Update("UPDATE g_baseball SET b_bat=#{b_bat},b_hit=#{b_hit},b_rbi=#{b_rbi},b_score=#{b_score},b_avg=#{b_avg},b_win=#{b_win},b_lose=#{b_lose},b_strike=#{b_strike},b_ip=#{b_ip},b_er=#{b_er},b_era=#{b_era} WHERE b_seq=#{b_seq}")
+	@Update("UPDATE g_baseball SET b_bat=#{b_bat},b_hit=#{b_hit},b_rbi=#{b_rbi},b_score=#{b_score},b_avg=NVL(#{b_hit}/DECODE(#{b_bat},0,NULL,#{b_bat}),0),b_win=#{b_win},b_lose=#{b_lose},b_strike=#{b_strike},b_ip=#{b_ip},b_er=#{b_er},b_era=NVL((#{b_er}*9)/DECODE(#{b_ip},0,NULL,#{b_ip}),0) WHERE b_seq=#{b_seq}")
 	public void updateBase(BaseCommand base);
 	@Update("UPDATE g_basketball SET b_score=#{b_score},b_assist=#{b_assist},b_rebound=#{b_rebound},b_steel=#{b_steel},b_block=#{b_block},b_3point=#{b_3point} WHERE b_seq=#{b_seq}")
 	public void updateBasket(BasketCommand basket);
@@ -36,12 +36,9 @@ public interface TotalTypeMapper {
 	
 	
 	// 개인별 통합랭킹 리스트
-	@Select("SELECT * FROM G_MEMBER M, (SELECT id,sum(b_bat) b_bat,sum(b_hit) b_hit,sum(b_rbi) b_rbi,sum(b_score) b_score, sum(b_win) b_win, sum(b_lose) b_lose, sum(b_strike) b_strike, sum(b_ip) b_ip, sum(b_er) b_er FROM g_baseball GROUP BY ROLLUP(id)) B WHERE M.ID=B.ID")
-	public List<BaseCommand> listBase();
-	@Select("SELECT * FROM G_MEMBER M, (SELECT id,sum(b_3point) AS b_3point,sum(b_assist) b_assist,sum(b_steel) b_steel,sum(b_score) b_score,sum(b_block) b_block,sum(b_rebound) b_rebound FROM g_basketball GROUP BY ROLLUP(id)) B WHERE M.ID=B.ID ORDER BY b_score DESC")
-	public List<BasketCommand> listBasket();	
-	@Select("SELECT * FROM G_MEMBER M, (SELECT id,sum(f_goal) AS f_goal,sum(f_assist) f_assist,sum(f_shoot) f_shoot,sum(f_attack) f_attack FROM g_football GROUP BY ROLLUP(id)) F WHERE M.ID=F.ID ORDER BY f_attack DESC")
-	public List<FootCommand> listFoot();
+	public List<BaseCommand> listBase(Map<String, Object> map);
+	public List<BasketCommand> listBasket(Map<String, Object> map);	
+	public List<FootCommand> listFoot(Map<String, Object> map);
 	
 	
 	
